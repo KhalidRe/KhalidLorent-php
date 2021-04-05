3c573c129009cc3c1f68282b652ad749658e3400
KhalidRe <khaledtumah@gmail.com>
2021-04-05 11:15:16 +0200

    fix

--- Form.php
+++ Form.php
@@ -1,12 +1,6 @@
-<?php 
-require "Connect.php"
-
-$query = "SELECT name FROM animals WHERE category = :Fisk";
-$statement = $dbh->prepare($query, array(PDO::FETCH_ASSOC));
-$statement->execute(array(':category' => "fisk"));
-$result = $statement->fetchAll();
-foreach ($result as $zoo) {
-echo $zoo['name'] . "<br />";
+<?php
+$query = "SELECT name FROM animals";
+foreach ($dbh->query($query) as $zoo) {
+echo $zoo['name'] . "<br/>";
 }
-
 ?>
